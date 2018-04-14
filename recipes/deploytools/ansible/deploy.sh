151fd0eb2a4702b0c1257243610f070f75b0ff5a
Tim Muller <tim@byte.nl>
2018-04-14 15:34:27 +0000

    Shared files and dirs

--- recipes/deploytools/ansible/deploy.sh
+++ recipes/deploytools/ansible/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
-
-ansible-playbook $BASEDIR/deploy.yml  -i $BASEDIR/hosts  --extra-vars "pwd=$(pwd)" $*
+export ANSIBLE_COW_SELECTION=tux
+ansible-playbook $BASEDIR/deploy.yml  -i $BASEDIR/hosts  --extra-vars "pwd=$(pwd)" --extra-vars "@$BASEDIR/config.yml" $*
