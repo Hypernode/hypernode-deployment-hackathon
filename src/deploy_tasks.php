45315dbb904ff6c290d96860d5066853976c48a4
Jeroen Boersma <jeroen@srcode.nl>
2018-04-14 15:14:28 +0000

    Optimized and set application

--- src/deploy_tasks.php
+++ src/deploy_tasks.php
@@ -3,8 +3,10 @@
  * Register build tasks to Magento even when Magento is not installed yet.
  */
 
+use \Hypernode\Deployment\Tasks\Deploy\Builtin\SetupUpgrade;
+
 if (PHP_SAPI == 'cli') {
-    \Hypernode\Deployment\Tasks\Deploy\DeployTaskList::registerTask(
-        new \Hypernode\Deployment\Tasks\Deploy\Builtin\SetupUpgrade(100)
+    \Hypernode\Deployment\Tasks\Deploy\DeployTaskList::registerTasks(
+        new SetupUpgrade(100)
     );
 }
