db51a77b3d03b6b2bbfe7fd0974d62d132c41d3e
Robin van Raan <robin.vanraan@jaagers.com>
2018-04-26 13:50:05 +0200

    reset formatting to original format and updated script to amend new changes

--- recipes/deploytools/deployer/deploy.php
+++ recipes/deploytools/deployer/deploy.php
@@ -6,29 +6,41 @@ require_once 'recipe/common.php';
 
 // Configuration
 //Shared files are files that will be shared between each build
-set('shared_files', [
-    'app/etc/env.php',
-    'var/.maintenance.ip',
-]);
+set(
+    'shared_files',
+    [
+        'app/etc/env.php',
+        'var/.maintenance.ip',
+    ]
+);
 
 //Shared dirs are directories that will be shared between each build. Please dont share  the whole var dir.
-set('shared_dirs', [
-    'var/log',
-    'var/backups',
-    'pub/media',
-]);
+set(
+    'shared_dirs',
+    [
+        'var/log',
+        'var/backups',
+        'pub/media',
+    ]
+);
 
 //Writeable dirs are dirs that will be made writeable
-set('writable_dirs', [
-    'var',
-    'pub/static',
-    'pub/media',
-]);
+set(
+    'writable_dirs',
+    [
+        'var',
+        'pub/static',
+        'pub/media',
+    ]
+);
 
 //clear_paths will be cleared after deploying
-set('clear_paths', [
-    'var/cache/*',
-]);
+set(
+    'clear_paths',
+    [
+        'var/cache/*',
+    ]
+);
 
 //Basic hypernode configuration settings for deployer
 set('writable_mode', 'chmod');
@@ -38,65 +50,93 @@ set('public_folder_staging', '~/staging');
 
 //Composer installation parameters in deploy:vendors command (`{{bin/composer}} {{composer_options}}`)
 set('composer_action', 'install');
-set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
+set(
+    'composer_options',
+    '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader'
+);
 
 //Load the host information from .hypernode.hosts.yaml
-inventory('.hypernode.hosts.yaml');
+inventory('.hypernode.hosts.yml');
 
 // Tasks
 //TESTS
-task('tests:run', function () {
-    //Todo: implement your test routine here
-})->local();
+task(
+    'tests:run',
+    function () {
+        //Todo: implement your test routine here
+    }
+)->local();
 
 //BUILD
-task('build', function () {
-    set('deploy_path', __DIR__);
-    set('release_path', __DIR__);
-    set('public_dir', '/');
+task(
+    'build',
+    function () {
+        set('deploy_path', __DIR__);
+        set('release_path', __DIR__);
+        set('public_dir', '/');
 
-    invoke('deploy:vendors');
-    invoke('build:magento');
-})->local();
+        invoke('deploy:vendors');
+        invoke('build:magento');
+    }
+)->local();
 
-task('build:magento', function () {
-    run("{{bin/php}} {{release_path}}/bin/magento hypernode:build");
-});
+task(
+    'build:magento',
+    function () {
+        //TODO: log the output
+        run("{{bin/php}} {{release_path}}/bin/magento hypernode:build");
+    }
+);
 
 //DEPLOY
-task('deploy:magento', function () {
-    run("{{bin/php}} {{release_path}}/bin/magento hypernode:deploy");
-});
+task(
+    'deploy:magento',
+    function () {
+        //TODO: log the output
+        run("{{bin/php}} {{release_path}}/bin/magento hypernode:deploy");
+    }
+);
 
 //UPLOAD
-task('upload', function () {
-    upload(__DIR__.'/', '{{release_path}}');
-});
+task(
+    'upload',
+    function () {
+        upload(__DIR__ . '/', '{{release_path}}');
+    }
+);
 
 //SYMLINK
-task('symlink:hypernode', function () {
-    $stage = get('stage');
-    $public_folder = get('public_folder_'.$stage);
-    if (!$public_folder) throw new Exception(sprintf("Public folder not found for stage: %s", $stage));
+task(
+    'symlink:hypernode',
+    function () {
+        $stage = get('stage');
+        $public_folder = get('public_folder_' . $stage);
+        if (!$public_folder) {
+            throw new Exception(sprintf("Public folder not found for stage: %s", $stage));
+        }
 
-    set('public_folder', $public_folder);
-    run("{{bin/symlink}} -f {{deploy_path}}/current/pub {{public_folder}}");
-});
+        set('public_folder', $public_folder);
+        run("{{bin/symlink}} -f {{deploy_path}}/current/pub {{public_folder}}");
+    }
+);
 
 //HOOKS
 after('deploy:symlink', 'symlink:hypernode');
 
 //DEPLOY
-task('deploy', [
-    'deploy:info',
-    'deploy:prepare',
-    'deploy:release',
-    'build',
-    'upload',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:symlink',
-    'deploy:magento',
-    'cleanup',
-    'success'
-]);
+task(
+    'deploy',
+    [
+        'deploy:info',
+        'deploy:prepare',
+        'deploy:release',
+        'build',
+        'upload',
+        'deploy:shared',
+        'deploy:writable',
+        'deploy:symlink',
+        'deploy:magento',
+        'cleanup',
+        'success',
+    ]
+);
